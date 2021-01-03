56364aebf19b57be359140d297355f0989025657
Aleksandr Serebrovskiy <aleksandr-web@yandex.ru>
2021-01-03 20:52:29 +0300

    add account-login and refactoring

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './NavBar.css';
+import AccountLogin from '../UI/AccountLogin'
 
 
 function NavBar() {
   return (
     <nav className="menu">
       <div className="menu__container">
-        <from className="menu__account">
-          <input type="text" className="menu__account-input" name="inputLogin" />
-          <input type="text" className="menu__account-input" name="inputPassword" />
-          <button type="button" className="menu__account-button">войти</button>
-        </from>
+        <AccountLogin />
         <NavLink exact to="/reviews" className="menu__link" activeClassName="menu__link_active">Обзоры</NavLink>
         <NavLink to="/rating" className="menu__link" activeClassName="menu__link_active">Рейтинг</NavLink>
         <NavLink to="/premieres" className="menu__link" activeClassName="menu__link_active">Премьеры</NavLink>
